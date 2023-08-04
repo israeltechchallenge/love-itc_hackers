b3f48fb3d8ca0a84417b2e193128f64e1ec1e229
arielamos <arielamos@hotmail.com>
2023-08-04 11:09:56 +0300

    Ariel's test

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import AboutHeader from "./feature/AboutHeader";
+import AboutSection from "./feature/AboutSection";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <AboutHeader />
+      <AboutSection />
+    </div>
+  );
+}
+
+export default App;
